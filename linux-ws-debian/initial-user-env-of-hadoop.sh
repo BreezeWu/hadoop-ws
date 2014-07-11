0d54f1145064b831e2b0ec3659a53594d09fb680
Breeze Wu <dawningbreezee@gmail.com>
2014-07-11 17:40:06 +0800

    linux-ws of debian

---
+++ linux-ws-debian/initial-user-env-of-hadoop.sh
@@ -0,0 +1,5 @@
+sudo ntfs-3g /dev/sda7 /mnt/win-f
+python ~/link-goagent/local/proxy.py
+
+# ln -s /home/hadoop/workspace_github/hadoop-ws/debian-ws/initial-user-env-of-hadoop.sh /home/hadoop/initial-user-env-of-hadoop.sh
+
