5991b915aa8e53eb27b62d109bf650683b8451d0
Breeze Wu <dawningbreezee@gmail.com>
2014-07-16 12:48:40 +0800

    将"k-means 及其评估"封装为函数,以便多次运行,以找出最小WSSSE的k值

---
+++ spark-ws/README.md
@@ -0,0 +1,15 @@
+spark-ws目录规划
+
+HDFS上的根目录 /user/spark
+hdfs dfs -ls -R /user/spark/
+=========
+profiling	算法性能输出目录
+dm/model	Data Mining结果输出: 模型
+dm/profiling	Data Mining过程性能
+=========
+
+=========
+
+=========
+
+=========
--- spark-ws/demo-spark-03-mllib-02-kmeans.scala
+++ spark-ws/demo-spark-03-mllib-02-kmeans.scala
@@ -20,7 +20,7 @@ val rddFromHive = hiveContext.hql("SELECT cons_id, ym, volume_per_month FROM big
 // 转换为vector的方法是
 val parsedData = rddFromHive.map(x => Array(
 		x(0).toString.toDouble,x(1).toString.toDouble,x(2).toString.toDouble)
-	).map(x => Vectors.dense(x))
+	).map(x => Vectors.dense(x)).cache()
 
 // end ------------------------------ 从hive取数据
 // end =========================================== 将用户数据,用电量数据,欠费等信息组合起来
@@ -38,4 +38,70 @@ val clusters = KMeans.train(parsedData, numClusters, numIterations)
 val WSSSE = clusters.computeCost(parsedData)
 println("Within Set Sum of Squared Errors = " + WSSSE)
 
+val sparkRoot = "/user/spark/"
+val dmProfiling = sparkRoot + "profiling/"
+val kmeansProfiling = dmProfiling + "k-means"
+
+// numClusters,numIterations,
+// 分隔符
+val delimiter:String = 0x01.toChar.toString
+
+val outputLine = s"$numClusters$delimiter$numIterations$delimiter$WSSSE"
+val outputData = List(outputLine)
+val distData = sc.parallelize(outputData)
+distData.saveAsTextFile(kmeansProfiling)
+
+// hdfs查看结果
+// 	hdfs dfs -ls /user/spark/profiling/k-means
+// 	hdfs dfs -cat /user/spark/profiling/k-means/*
+// 清空数据 hdfs dfs -rm -r /user/spark/profiling/k-means/
+
 // end =========================================== k-means 及其评估
+
+
+// ----------------------------------------------------------------------------
+// 将"k-means 及其评估"封装为函数,以便多次运行
+// 定义函数
+//def tryKMeans(k: Int, maxIterations: Int = 20, runs: Int = 1, initializationMode: String)
+def tryKMeans(mixK: Int = 2, maxK: Int) = {
+	// 基础变量
+	var outputDataList = List[String]()
+	outputDataList = Nil
+
+	// Cluster the data into two classes using KMeans
+	val numClusters = 2
+	val numIterations = 20
+	
+	// ----------------- 执行K-means算法
+	// val mixK = 2,maxK = 10
+	val range = Range(mixK, maxK)
+	for(k <- range) {
+		val clusters = KMeans.train(parsedData, k, numIterations)
+
+		// Evaluate clustering by computing Within Set Sum of Squared Errors
+		val WSSSE = clusters.computeCost(parsedData)
+
+		// Profiling
+		val delimiter:String = 0x01.toChar.toString	// 分隔符 Ctrl+A
+		val outputLine = s"$k$delimiter$numIterations$delimiter$WSSSE"
+		outputDataList = outputLine.toString :: outputDataList
+	}
+
+	// ----------------- 写入文件
+	val sparkRoot = "/user/spark/"
+	val dmProfiling = sparkRoot + "profiling/"
+	val kmeansProfiling = dmProfiling + "k-means"
+
+	val distData = sc.parallelize(outputDataList)
+	distData.saveAsTextFile(kmeansProfiling)
+	outputDataList
+}
+
+tryKMeans(2,10)
+
+
+
+
+
+
+
