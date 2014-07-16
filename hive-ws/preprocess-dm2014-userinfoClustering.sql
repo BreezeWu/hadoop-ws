e713b95a48a782073c440cc6ba7ab066febfe997
Breeze Wu <dawningbreezee@gmail.com>
2014-07-16 15:25:31 +0800

    dm2014,用户信息聚类的数据预处理

---
+++ hive-ws/preprocess-dm2014-userinfoClustering.sql
@@ -0,0 +1,8 @@
+-- ----------------------------------------------------------------------------
+-- 1. 原始数据来自 ~/workspace_github/hadoop-ws/ws-setup/mysql-relative 目录下create-tables-of-dm2014.sql, data.2014-06-16.utf-8.tar.gz.NULLfile,import-csv-into-dm2014.sql,mysql-user-db.sql等脚本
+-- 2. 该数据预处理为spark脚本 demo-spark-03-mllib-02-kmeans.scala 使用
+-- 3. 脚本执行有两种思路,(1)直接在hive中执行; (2) 在spark-shell中利用 spark SQL执行
+
+
+
+
