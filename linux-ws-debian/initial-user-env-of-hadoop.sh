7834b61c74f9e85f3d2b970292e6c4a676dfe2de
Breeze Wu <dawningbreezee@gmail.com>
2014-07-16 10:37:41 +0800

    给eth0添加静态IP

--- linux-ws-debian/initial-user-env-of-hadoop.sh
+++ linux-ws-debian/initial-user-env-of-hadoop.sh
@@ -3,7 +3,7 @@ cd ~/link-goagent/local
 python ~/link-goagent/local/proxy.py
 
 # 设置网络
-# sudo ifconfig eth0 inet up 192.168.2.1 netmask 255.255.255.0
+# sudo ifconfig eth0 inet up 192.0.2.1 netmask 255.255.255.0
 
 # 初始化脚本,便于编辑后可通过git直接同步到github
 # ln -s /home/hadoop/workspace_github/hadoop-ws/linux-ws-debian/initial-user-env-of-hadoop.sh /home/hadoop/initial-user-env-of-hadoop.sh
