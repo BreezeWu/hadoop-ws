46dfbbc8bdc2346a4db9625e695af4b42db4ca8a
Breeze Wu <dawningbreezee@gmail.com>
2014-07-16 08:33:11 +0800

    initial-user-env-of-hadoop.sh 配置eth0为静态网络192.0.2.1

--- linux-ws-debian/initial-user-env-of-hadoop.sh
+++ linux-ws-debian/initial-user-env-of-hadoop.sh
@@ -1,5 +1,9 @@
 sudo ntfs-3g /dev/sda7 /mnt/win-f
+cd ~/link-goagent/local
 python ~/link-goagent/local/proxy.py
+
+# 设置网络
+# sudo ifconfig eth0 inet up 192.168.2.1 netmask 255.255.255.0
 
 # 初始化脚本,便于编辑后可通过git直接同步到github
 # ln -s /home/hadoop/workspace_github/hadoop-ws/linux-ws-debian/initial-user-env-of-hadoop.sh /home/hadoop/initial-user-env-of-hadoop.sh
