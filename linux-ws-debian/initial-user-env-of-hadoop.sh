63aa9109f7e368312364ac16c1f2b979b1824e96
Breeze Wu <dawningbreezee@gmail.com>
2014-07-23 10:41:45 +0800

    .bashrc

--- linux-ws-debian/initial-user-env-of-hadoop.sh
+++ linux-ws-debian/initial-user-env-of-hadoop.sh
@@ -1,7 +1,8 @@
 sudo ntfs-3g /dev/sda7 /mnt/win-f
 cd ~/link-goagent/local
-python ~/link-goagent/local/proxy.py
+python ~/link-goagent/local/proxy.py &
 
+# -----------------------------------------------------------------
 # 手动设置以太网
 # 	sudo ifconfig eth0 inet up 192.0.2.1 netmask 255.255.255.0
 # 手动设置无线网络
@@ -9,6 +10,11 @@ python ~/link-goagent/local/proxy.py
 # 禁用eth0
 # sudo ifconfig eth0 inet down
 
+# -----------------------------------------------------------------
+# 在linux下面访问魅族3手机
+
+
+# -----------------------------------------------------------------
 # 初始化脚本,便于编辑后可通过git直接同步到github
 # ln -s /home/hadoop/workspace_github/hadoop-ws/linux-ws-debian/initial-user-env-of-hadoop.sh /home/hadoop/initial-user-env-of-hadoop.sh
 # hadoop用户的.bashrc
