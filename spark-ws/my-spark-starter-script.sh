c3f2d7c66a8a951fcaa3219175c87c15a63cde87
Breeze Wu <dawningbreezee@gmail.com>
2014-07-23 08:36:03 +0800

    my-spark-starter-script.sh

--- spark-ws/my-spark-starter-script.sh
+++ spark-ws/my-spark-starter-script.sh
@@ -10,3 +10,5 @@ ${SPARK_HOME}/bin/spark-shell
 
 # 或者,直接如下方式
 #	MASTER=spark://<spark_hostname>:7077 SPARK_DRIVER_MEMORY=1g SPARK_EXECUTOR_MEMORY=1g spark-shell
+# 又如:  SPARK_DRIVER_MEMORY=1g SPARK_EXECUTOR_MEMORY=1g spark-shell
+
