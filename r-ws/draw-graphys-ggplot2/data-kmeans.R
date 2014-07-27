93ca8dbd3d4954e2e56f7150ef6265c97ec927ac
Breeze Wu <dawningbreezee@gmail.com>
2014-07-28 00:31:36 +0800

    ggplot2: preparing data for R

--- r-ws/draw-graphys-ggplot2/data-kmeans.R
+++ r-ws/draw-graphys-ggplot2/data-kmeans.R
@@ -6,6 +6,10 @@
 # 使用我的数据
 #    每次手动选择文件
 #	mydata = read.table(file.choose(), header=FALSE, sep=",")
+
+# 在R环境中,使用下面语句
+# 	source("~/workspace_github/hadoop-ws/r-ws/draw-graphys-ggplot2/data-kmeans.R")
+
 # *****************************************************************************
 # -----------------------------------------------------------------------------
 ## metrics
@@ -22,8 +26,9 @@ mymetrics
 labels_metrics <- c("k", "maxIterations", "WSSSE", "聚类开始时间", "聚类结束时间", "KMeansModel")
 names(mymetrics) <- labels_metrics
 
-# 将k转换为factor
-mymetrics$k.f <- as.factor(mymetrics$k)
+# 将k, maxIterations 转换为factor
+mymetrics$k <- as.factor(mymetrics$k)
+mymetrics$maxIterations <- as.factor(mymetrics$maxIterations)
 
 # 取第1,3列
 mymetrics[,1-3]
@@ -36,7 +41,8 @@ mymetrics[,1-3]
 myclustercenters = read.table("~/workspace_github/hadoop-ws/r-ws/data/clustercenters.csv", header=FALSE, sep=",")  # read table file ,首行无列名(header=FALSE)
 myclustercenters
 
-# vpm201301,vpm201302,vpm201303,vpm201304,vpm201305,vpm201306,vpm201307,vpm201308,vpm201309,vpm201310,vpm201311,vpm201312,vpm201401,vpm201402,vpm201403,vpm201404,vpm201405,vpm201406
+# -----------------------------------------------------------------------------
+# vpm vpm201301,vpm201302,vpm201303,vpm201304,vpm201305,vpm201306,vpm201307,vpm201308,vpm201309,vpm201310,vpm201311,vpm201312,vpm201401,vpm201402,vpm201403,vpm201404,vpm201405,vpm201406
 # 取前面18列,即从 201301~201406这18个月的月用点量
 vpm <- myclustercenters[, c(1:18)]
 
@@ -47,22 +53,45 @@ x.substr <- substr(x, 2, nchar(x))	# 从第二个字符截取
 vpm$V1 <- as.numeric(x.substr)
 rm(x)
 
-# 将行号变为一列
-rownums <- as.numeric(rownames(vpm))
-vpm <- data.frame(vpm, factor(rownums))	# 因子化 
-
 # 为vpm设置变量标签
 newcolnames <- c("201301", "201302", "201303", "201304", "201305", "201306", "201307", "201308", "201309", "201310", "201311", "201312", "201401", "201402", "201403", "201404", "201405", "201406")
 #newcolnames <- c("13/1", "13/2", "13/3", "13/4", "13/5", "13/6", "13/7", "13/8", "13/9", "13/10", "13/11", "13/12", "14/1", "14/2", "14/3", "14/4", "14/5", "14/6")
-names(vpm) <- c(newcolnames,"rowid")
+names(vpm) <- c(newcolnames)	# names(vpm.rowid) <- c(newcolnames,"rowid")
+rm(newcolnames)
+
+# -----------------------------------------------------------------------------
+# 生成 vpm.v (增加了rowid)
+# 将行号变为一列
+newcolumn <- factor(as.numeric(rownames(vpm)))
+vpm.rowid <- data.frame(vpm, newcolumn)	# 为什么这一句后,列明变为了 x201301?
+
+# 将新列的列名改为rowid
+# 重新设置列名
+newcolnames <- c("201301", "201302", "201303", "201304", "201305", "201306", "201307", "201308", "201309", "201310", "201311", "201312", "201401", "201402", "201403", "201404", "201405", "201406")
+names(vpm.rowid) <- c(newcolnames,"rowid")
 rm(newcolnames)
 
 # 横表变纵表
-
 library(reshape)
 #vpm.v <- melt(vpm ,  id = 'rowid')  				# variable -> value
-vpm.v <- melt(vpm ,  id = 'rowid', variable_name = 'colnames')		# colnames -> value
+vpm.v <- melt(vpm.rowid,  id = 'rowid', variable_name = 'ym')		# colnames/ym -> value
 
 # 画图的列
-vpm.v$ym <- as.factor(vpm.v$colnames)
+#vpm.v$ym <- as.factor(vpm.v$colnames)
+
+# -----------------------------------------------------------------------------
+#  行列转换
+#	vpm.t	从vpm进行转换
+#vpm.t <- t(vpm)			# 这种方法,值变为了 character vector
+vpm.t <- as.data.frame(t(vpm))	
+
+# 将 行名 成为新列 ym
+#vpm.t <- data.frame(vpm.t, factor(rownames(vpm)))	# 不需要因子化,所以使用下面的语句即可
+
+#vpm.t <- data.frame(vpm.t, rownames(vpm.t))	# 最后附加的列明是rownames.vpm.t. 所以改为下面两句
+ym <- rownames(vpm.t)
+vpm.t <- data.frame(vpm.t, ym)
+
+
+
 
--- r-ws/draw-graphys-ggplot2/kmeans-basic-visualizing-of-metrics.R
+++ r-ws/draw-graphys-ggplot2/kmeans-basic-visualizing-of-metrics.R
@@ -16,14 +16,18 @@
 # Bar Graphs		# mymetrics 数据画"Bar Graphs"是无意义的,只是为了演示语法
 require(ggplot2)
 ## Loading required package: ggplot2
-ggplot(mymetrics, aes(x = k)) + geom_bar()
+ggplot(mymetrics, aes(x = k)) + geom_bar()		# 默认计数是 range/30, 即 50/30=1.6
+ggplot(mymetrics, aes(x = k)) + geom_bar(binwidth = 1)	# 所有的计数都变为了1
+ggplot(mymetrics, aes(x = k)) + geom_bar(binwidth = 10)
+
 ggplot(mymetrics, aes(x = WSSSE)) + geom_bar()
+ggplot(mymetrics, aes(x = WSSSE)) + geom_bar(binwidth = 0.0000001)	# 这个不被接受!
 
 # -----------------------------------------------
 # 折线图
 library(ggplot2)
-qplot(k.f, WSSSE, data=mymetrics, geom=c("boxplot", "jitter"),
-      fill=cylinder, 
+qplot(k, WSSSE, data=mymetrics, geom=c("boxplot", "jitter"),
+      fill=k, 
       main="K效能图",
       xlab= "K",
         ylab="WSSSE")
