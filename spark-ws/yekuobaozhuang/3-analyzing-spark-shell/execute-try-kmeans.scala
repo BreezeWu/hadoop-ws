e2f555fd76fc8602480834a11902957917f52f51
Breeze Wu <dawningbreezee@gmail.com>
2014-09-22 09:48:28 +0800

    原始数据与百分比数据

--- spark-ws/yekuobaozhuang/3-analyzing-spark-shell/execute-try-kmeans.scala
+++ spark-ws/yekuobaozhuang/3-analyzing-spark-shell/execute-try-kmeans.scala
@@ -2,10 +2,13 @@
 // --------------------------
 // 聚类数据: (1) 只取参与计算的数据；(2) 转变为Vector, 构建matrices
 import org.apache.spark.mllib.linalg.Vectors
-val averageRdd = mappedRddData_percent.map(x => x.average).map(y => Vectors.dense(y.toArray)).cache()
-val averagepercentRdd = mappedRddData_percent.map(x => x.averagePercent).map(y => Vectors.dense(y.toArray)).cache()
 
-val data = averagepercentRdd // averageRdd
+// 取 平均值(年度月用电量)数据
+val averageRdd = mappedRddData_percent.map(x => x.average).map(y => Vectors.dense(y.toArray)).cache()
+// 取 平均值(年度月用电量)数据的百分比
+val averageRdd_percent = mappedRddData_percent.map(x => x.averagePercent).map(y => Vectors.dense(y.toArray)).cache()
+
+val data = averageRdd_percent // averageRdd
 
 val env = System.getenv()
 val pwd = env.get("PWD")
@@ -28,10 +31,16 @@ val resultWClusterCountInfo2HDFS = writeClusterCountInfo2LocalFile(resultCluster
 val perfectK = 50;
 val maxIterations = 20 // 当前没有生效
 
+val env = System.getenv()
+val pwd = env.get("PWD")
+
 // 1. 有效数据
 // (1) 单月数据
 val resultClusterCountInfo_Standalone = ComputeClusterCount_Standalone(data, perfectK, maxIterations)
-val resultWClusterCountInfo2HDFS_Standalone = writeClusterCountInfo2LocalFile(resultClusterCountInfo_Standalone, "yekuobaozhuang")
+// 中心点和metric信息
+val resultW2LocalFile_Standalone = writeAccount2LocalFile(resultClusterCountInfo_Standalone.account, "yekuobaozhuang", pwd)
+// 统计信息
+val resultWClusterCountInfo2HDFS_Standalone = writeClusterCountInfo2LocalFile(resultClusterCountInfo_Standalone, "yekuobaozhuang", pwd)
 // ----------------------------------------------------------------------------
 //  统一显示一下结果变量
 // 任务1
